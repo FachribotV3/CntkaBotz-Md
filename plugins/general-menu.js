b06af3ea429a98c56a0ba28c7e8d2c3c88b2377d
FachribotV3 <109334518+FachribotV3@users.noreply.github.com>
2022-07-19 14:46:47 +0700

    Update general-menu.js

--- plugins/general-menu.js
+++ plugins/general-menu.js
@@ -107,13 +107,13 @@ let handler = async (m, { conn, usedPrefix: _p }) => {
     //conn.sendHydrated(m.chat, text.trim(), conn.getName(conn.user.jid), await genProfile(conn, m), 'https://youtube.com/channel/UC0hs_I8N3JntK5vO6KogavQ', 'YouTube', null, null, [['Speedtest', _p + 'ping'], ['Owner', _p + 'owner']], m)
    // conn.sendMessage(m.chat, { video: { url: 'https://telegra.ph/file/c82d5c358495e8ef15916.mp4' }, gifPlayback: true, gifAttribution: ~~(Math.random() * 2), caption: text.trim(), footer: await conn.getName(conn.user.jid) , templateButtons: [{ quickReplyButton: { displayText: 'Speedtest', id: `${_p}ping` }}, { quickReplyButton: { displayText: 'Owner', id: `${_p}owner` }} ] })
    await conn.sendButton(m.chat, `*${wish()}, ${name} 👋*`, text.trim(), await (await fetch('https://telegra.ph/file/b9c3b2edd707cf6a490de.png')).buffer(), [['Donasi', _p + 'donasi'], ['Owner', _p + 'owner']], false, { quoted: ftroli, contextInfo: { externalAdReply: { showAdAttribution: true,
-     mediaUrl: 'https://youtu.be/JWHV8lPTzPs'
-     mediaType: 2, 
-     description:  '',  
-     title: wm2,
-     body: `${ingat()}`,
-     thumbnail: await (await fetch('https://telegra.ph/file/a9a6907db67ca1a0c0877.jpg')).buffer(),
-     sourceUrl: 'https://vt.tiktok.com/ZSdwokqe4/'
+    mediaUrl: `https://youtu.be/35w7z9QFLwY`
+    title: wm2,  
+    body: 'FachriBot By Fachri',
+    description: '', 
+    mediaType: 2, 
+    sourceUrl: 'http://github.com/Ipulpachri',
+    thumbnail: await (await fetch('https://telegra.ph/file/a9a6907db67ca1a0c0877.jpg')).buffer(), 
 }
 } })
 conn.sendFile(m.chat, './media/AUD-20220711-WA0077.mp3', 'AUD-20220711-WA0077.mp3', null, fkontak, true, {
