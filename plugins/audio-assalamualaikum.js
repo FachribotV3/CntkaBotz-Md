cedbcf5585510f313d981a93ac01a8393e20af1f
FachribotV3 <109334518+FachribotV3@users.noreply.github.com>
2022-07-20 18:33:13 +0700

    Update audio-assalamualaikum.js

--- plugins/audio-assalamualaikum.js
+++ plugins/audio-assalamualaikum.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn }) => {
 let vn = './media/Assalamualaikum.mp3'
 conn.sendFile(m.chat, vn, 'Assalamualaikum.mp3', null, fkontak, true, {
 type: 'audioMessage', 
-ptt: true, contextInfo:{ externalAdReply: {title: 'Wa'alaikumussalam Kak', body: `${pickRandom(['Simple Bot WhatsApp', 'Create By Fachri'])}`, sourceUrl: 'https://github.com/Ipulpachri', thumbnail: await (await fetch('https://telegra.ph/file/63c668962b7abcc95b394.jpg')).buffer(),}} 
+ptt: true, contextInfo:{ externalAdReply: {title: 'Waalaikumussalam Kak', body: `${pickRandom(['Simple Bot WhatsApp', 'Create By Fachri'])}`, sourceUrl: 'https://github.com/Ipulpachri', thumbnail: await (await fetch('https://telegra.ph/file/63c668962b7abcc95b394.jpg')).buffer(),}} 
 })
     // conn.sendButton(m.chat, 
     //`*Hi, ${name} 👋*\n\n`, 
