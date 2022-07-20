f281058d5f1ca85f0ba1c1e1b733b1d0824890c1
FachribotV3 <109334518+FachribotV3@users.noreply.github.com>
2022-07-20 18:32:37 +0700

    Update audio-assalamualaikum.js

--- plugins/audio-assalamualaikum.js
+++ plugins/audio-assalamualaikum.js
@@ -5,13 +5,19 @@ let handler = async (m, { conn }) => {
 let vn = './media/Assalamualaikum.mp3'
 conn.sendFile(m.chat, vn, 'Assalamualaikum.mp3', null, fkontak, true, {
 type: 'audioMessage', 
-ptt: true, contextInfo: { externalAdReply: { showAdAttribution: true,
-title: 'Waalaikumussalam Kak😇',
-body: `${pickRandom(['Simple Bot WhatsApp', 'Create By Fachri'])}`, 
-sourceUrl: 'https://github.com/Ipulpachri', 
-thumbnail: await (await fetch('https://telegra.ph/file/63c668962b7abcc95b394.jpg')).buffer(),
+ptt: true, contextInfo:{ externalAdReply: {title: 'Wa'alaikumussalam Kak', body: `${pickRandom(['Simple Bot WhatsApp', 'Create By Fachri'])}`, sourceUrl: 'https://github.com/Ipulpachri', thumbnail: await (await fetch('https://telegra.ph/file/63c668962b7abcc95b394.jpg')).buffer(),}} 
+})
+    // conn.sendButton(m.chat, 
+    //`*Hi, ${name} 👋*\n\n`, 
+  //  text.trim(), './media/marin.jpg', [
+// [`Speedtest`, `${_p}ping`],
+// [`Owner`, `${_p}owner`]
+//], m, {asLocation: true})
+  } catch (e) {
+    m.reply('An error occurred')
+    throw e
+  }
 }
-} })
 
 handler.help = ['assalamualaikum']
 handler.tags = ['sound']
