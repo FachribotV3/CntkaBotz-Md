e1be03f42d29fdf524b7262d9a34ce35a7727fec
FachribotV3 <109334518+FachribotV3@users.noreply.github.com>
2022-07-20 17:56:05 +0700

    Update misc-owner.js

--- plugins/misc-owner.js
+++ plugins/misc-owner.js
@@ -1,8 +1,8 @@
 function handler(m) {
   let data = global.owner.filter(([id, isCreator]) => id && isCreator)
   let ftroli = {key: {fromMe: false,"participant":"0@s.whatsapp.net", "remoteJid": "status@broadcast"}, "message": {orderMessage: {itemCount: 1,status: 200, surface: 200, message: `                   「 MY OWNER 」            `, orderTitle: 'FachriBot', sellerJid: '0@s.whatsapp.net'}}}
-  this.sendContact(m.chat, data.map(([id, name]) => [id, name]), { quoted: ftroli, contextInfo:{ externalAdReply: {title: 'Ini nomor orang ganteng', body: `${pickRandom(['Simple Bot WhatsApp', 'Create By Fachri'])}`, sourceUrl: 'https://github.com/Ipulpachri', thumbnail: await (await fetch('https://telegra.ph/file/63c668962b7abcc95b394.jpg')).buffer(),
-}
+  this.sendContact(m.chat, data.map(([id, name]) => [id, name]), false, { quoted: ftroli, contextInfo:{ externalAdReply: {title: 'Ini nomor orang ganteng', body: `${pickRandom(['Simple Bot WhatsApp', 'Create By Fachri'])}`, sourceUrl: 'https://github.com/Ipulpachri', thumbnail: await (await fetch('https://telegra.ph/file/63c668962b7abcc95b394.jpg')).buffer(),}}
+})
 
 handler.alias = ['owner']
 handler.command = /^(owner)$/i
