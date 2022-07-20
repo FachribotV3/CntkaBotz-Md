1ce753b63b0d2e6b9f0db0fc3d465a289a623642
FachribotV3 <109334518+FachribotV3@users.noreply.github.com>
2022-07-20 18:47:55 +0700

    Update audio-assalamualaikum.js

--- plugins/audio-assalamualaikum.js
+++ plugins/audio-assalamualaikum.js
@@ -5,18 +5,16 @@ let handler = async (m, { conn }) => {
 let vn = './media/Assalamualaikum.mp3'
 conn.sendFile(m.chat, vn, 'Assalamualaikum.mp3', null, fkontak, true, {
 type: 'audioMessage', 
-ptt: true, contextInfo:{ externalAdReply: {title: 'Waalaikumussalam Kak', body: `${pickRandom(['Simple Bot WhatsApp', 'Create By Fachri'])}`, sourceUrl: 'https://github.com/Ipulpachri', thumbnail: await (await fetch('https://telegra.ph/file/63c668962b7abcc95b394.jpg')).buffer(),}} 
-})
-    // conn.sendButton(m.chat, 
-    //`*Hi, ${name} 👋*\n\n`, 
-  //  text.trim(), './media/marin.jpg', [
-// [`Speedtest`, `${_p}ping`],
-// [`Owner`, `${_p}owner`]
-//], m, {asLocation: true})
-  } catch (e) {
-    m.reply('An error occurred')
-    throw e
-  }
+ptt: true , contextInfo: { showAdAttribution: true,
+         externalAdReply: { 
+            mediaType:  1,
+            mediaUrl: 'https://wa.me/17608914335',
+            title: 'Ada Apa Ya Kak?',
+          body: wm,
+         sourceUrl: 'http://github.com/Ipulpachri', thumbnail: await( await fetch('https://telegra.ph/file/a9a6907db67ca1a0c0877.jpg')).buffer()
+         } 
+      } 
+   })
 }
 
 handler.help = ['assalamualaikum']
@@ -26,28 +24,3 @@ handler.command = new RegExp
 handler.fail = null
 handler.exp = 100
 export default handler
-
-function wish() {
-    let wishloc = ''
-  const time = moment.tz('Asia/Kolkata').format('HH')
-  wishloc = ('Hi')
-  if (time >= 0) {
-    wishloc = ('Selamat DiniHari')
-  }
-  if (time >= 4) {
-    wishloc = ('Selamat Pagi Kak')
-  }
-  if (time >= 12) {
-    wishloc = ('Selamat Siang Kak')
-  }
-  if (time >= 18) {
-    wishloc = ('️Selamat Sore Kak')
-  }
-  if (time >= 23) {
-    wishloc = ('Selamat Malam Kak')
-  }
-  return wishloc
-    }
- function pickRandom(list) {
-  return list[Math.floor(Math.random() * list.length)]
-}
