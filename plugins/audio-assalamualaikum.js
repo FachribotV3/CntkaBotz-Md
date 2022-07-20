379a622b3b4facdf25e7bdb70ce0d9d6d09a4fb4
FachribotV3 <109334518+FachribotV3@users.noreply.github.com>
2022-07-20 18:48:44 +0700

    Update audio-assalamualaikum.js

--- plugins/audio-assalamualaikum.js
+++ plugins/audio-assalamualaikum.js
@@ -5,11 +5,11 @@ let handler = async (m, { conn }) => {
 let vn = './media/Assalamualaikum.mp3'
 conn.sendFile(m.chat, vn, 'Assalamualaikum.mp3', null, fkontak, true, {
 type: 'audioMessage', 
-ptt: true , contextInfo: { showAdAttribution: true,
+ptt: true, contextInfo: { showAdAttribution: true,
          externalAdReply: { 
             mediaType:  1,
             mediaUrl: 'https://wa.me/17608914335',
-            title: 'Ada Apa Ya Kak?',
+            title: 'Waalaikumussalam kak',
           body: wm,
          sourceUrl: 'http://github.com/Ipulpachri', thumbnail: await( await fetch('https://telegra.ph/file/a9a6907db67ca1a0c0877.jpg')).buffer()
          } 
