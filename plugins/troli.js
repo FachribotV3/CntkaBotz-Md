de75851a61ea181c08bd30dc032e68e0026dacd4
FachribotV3 <109334518+FachribotV3@users.noreply.github.com>
2022-07-23 19:20:10 +0700

    Update troli.js

--- plugins/troli.js
+++ plugins/troli.js
@@ -5,23 +5,22 @@ const {
     Mimetype
 } = (await import("@adiwajshing/baileys")).default;
  const anu = {
-	 key : {
-                           participant : '0@s.whatsapp.net'
-                         },
-        message: {
-                     orderMessage: {
-                             itemCount : 404,
-                             itemCoun : 404,
-                             surface : 404,
-                             message: 'UwU',
-                             orderTitle: 'B',
-                             thumbnail: fs.readFileSync('./thumbnail.jpg') 
-                             sellerJid: '0@s.whatsapp.net',
+	key : {
+                          participant : '0@s.whatsapp.net'
+                        },
+       message: {
+                    orderMessage: {
+                            itemCount : 404,
+                            itemCoun : 404,
+                            surface : 404,
+                            message: 'uwu',
+                            orderTitle: 'B',
+                            thumbnail: fs.readFileSync('./thumbnail.jpg'), 
+                            sellerJid: '0@s.whatsapp.net'
           
-                           }
-                         }
-                       }
-
+                          }
+                        }
+                      }
 conn.sendMessage(m.chat,{text:wm}, {quoted: anu})
 }
 
